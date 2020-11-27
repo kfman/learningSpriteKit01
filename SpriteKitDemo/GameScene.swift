8fdbdb41d8e1aa57dfeccb8f930a84b10f8947b0
KFischer(M13) <klaus.fischer@eloware.com>
2020-11-27 11:37:50 +0100

    Direction working angle buggy

--- SpriteKitDemo/GameScene.swift
+++ SpriteKitDemo/GameScene.swift
@@ -6,83 +6,84 @@
 //
 
 import SpriteKit
-import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    let myFirstNode = SKNode()
+    let myFirstSpriteNode = SKSpriteNode(color:UIColor.red, size: CGSize(width: 200.0, height: 200.0))
+    let myBlueSpriteNode = SKSpriteNode(color:UIColor.blue, size: CGSize(width: 100.0, height: 100.0))
+    let spaceship = SKSpriteNode(imageNamed: "spaceship")
     
     override func didMove(to view: SKView) {
+        addChild(myFirstNode)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        myFirstSpriteNode.position = CGPoint(x: frame.midX - 100, y: frame.midY - 150)
+        myFirstSpriteNode.anchorPoint = CGPoint.zero
+        addChild(myFirstSpriteNode)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        spaceship.size = CGSize(width: 100.0, height: 100.0)
+        spaceship.zPosition = 1
+        myFirstSpriteNode.addChild(spaceship)
+        myBlueSpriteNode.position = CGPoint(x:myFirstSpriteNode.size.width / 2 , y:myFirstSpriteNode.size.height / 2)
+        myBlueSpriteNode.zPosition = 2
+        myFirstSpriteNode.addChild(myBlueSpriteNode)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+
+        if let touch = touches.first{
+            var angle = .pi + -1 * atan2(spaceship.position.x - (touch.location(in: myFirstSpriteNode).x), spaceship.position.y - (touch.location(in: myFirstSpriteNode).y))
+               
+            let spaceAngle = spaceship.zRotation
+            if angle - spaceAngle > .pi { angle -= 2 * .pi }
+            if angle - spaceAngle < -1 * .pi {angle -= 2 * .pi}
+            
+            let duration = abs(TimeInterval(Float((angle - spaceAngle / .pi) * 0.3)))
+            
+        spaceship.run(
+            SKAction.sequence([
+                SKAction.rotate(toAngle: angle, duration: duration),
+                SKAction.move(to: (touch.location(in: myFirstSpriteNode)), duration: 1.0)
+            ]))
+         }
+//        if !spaceship.hasActions(){
+//        spaceship.run(
+//            SKAction.repeatForever(
+//            SKAction.sequence([
+//            SKAction.rotate(byAngle: .pi / -4, duration: 0.125),
+//            SKAction.move(to:CGPoint(x: myFirstSpriteNode.size.width,y: myFirstSpriteNode.size.height),duration: 2.0),
+//            SKAction.rotate(byAngle: .pi, duration: 0.5),
+//            SKAction.move(to:CGPoint.zero,duration: 2.0),
+//                SKAction.rotate(byAngle: .pi * 3 / -4, duration: 0.375)
+//        ])))
+//        }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+//        myFirstTexturedSpriteNode.run(SKAction.move(to: CGPoint(x: myFirstSpriteNode.size.width,y: myFirstSpriteNode.size.height), duration: 2.0))
+//
+//        myBlueSpriteNode.run(SKAction.rotate(byAngle: .pi, duration: 2.0))
+//        myFirstTexturedSpriteNode.run(SKAction.move(to: CGPoint(x: myFirstSpriteNode.size.width,y: myFirstSpriteNode.size.height),duration: 2.0)) {
+//            self.myFirstTexturedSpriteNode.position = CGPoint.zero
+//        }
+//
+//        if !myBlueSpriteNode.hasActions(){
+//            myBlueSpriteNode.run(SKAction.repeatForever(
+//                                    SKAction.rotate(byAngle: .pi, duration:  2.0)))
+//            myBlueSpriteNode.run(SKAction.group([SKAction.rotate(byAngle: .pi, duration:  2.0),
+//                                                 SKAction.scale(by: 0.9, duration: 2.0)))
+//            myBlueSpriteNode.run(SKAction.sequence([
+//                                                    SKAction.rotate(byAngle: .pi, duration:  0.5),
+//                                                    SKAction.scale(by: 0.9, duration: 2.0)
+//                                                 ]))
+//            }
+//        else{
+//            myBlueSpriteNode.removeAllActions()
+//        }
+//
+//        if let _ = myFirstTexturedSpriteNode.action(forKey: "Rotation"){
+//            myFirstTexturedSpriteNode.removeAction(forKey: "Rotation")
+//        }
+//        else{
+//        myFirstTexturedSpriteNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: .pi, duration: 2.0)), withKey: "Rotation")
+//        }
     }
 }
